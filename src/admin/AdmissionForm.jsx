709356eeb99c336d3d728b23559c37522f766d7c
Akshay Kumar <akshaykumar@Akshays-MacBook-Air.local>
2026-03-20 09:17:51 +0530

    removing errro state in admission form

--- src/admin/AdmissionForm.jsx
+++ src/admin/AdmissionForm.jsx
@@ -270,64 +270,56 @@ export default function AdmissionForm() {
   const [search, setSearch] = useState("");
   const [students, setStudents] = useState([]);
   const [selectedStudent, setSelectedStudent] = useState(null);
-  const [errors, setErrors] = useState({});
+  const [debouncedSearch, setDebouncedSearch] = useState("");
 
-  // 🔍 SEARCH API
+
   useEffect(() => {
-    if (search.length > 1) {
-      fetch(`http://localhost:5001/api/students/search?q=${search}`)
-        .then(res => res.json())
-        .then(data => setStudents(data));
-    } else {
-      setStudents([]);
-    }
-  }, [search]);
+  const timer = setTimeout(() => {
+    setDebouncedSearch(search);
+  }, 400); // 400ms delay
 
-  // 🔥 INPUT CHANGE
-  const handleChange = (e) => {
-    const { name, value, type, checked } = e.target;
+  return () => clearTimeout(timer);
+}, [search]);
 
+  //  SEARCH API
+useEffect(() => {
+  if (debouncedSearch.length > 1) {
+    fetch(`http://localhost:5001/api/students/search?q=${debouncedSearch}`)
+      .then(res => res.json())
+      .then(data => setStudents(data));
+  } else {
+    setStudents([]);
+  }
+}, [debouncedSearch]);
+
+  //  INPUT CHANGE
+ const handleChange = (e) => {
+  const { name, value, type, checked } = e.target;
+
+  if (name === "paymentStatus" && value === "unpaid") {
     setForm({
       ...form,
-      [name]: type === "checkbox" ? checked : value,
+      paymentStatus: value,
+      fee: "",
+      paymentMode: "",
     });
-  };
-
-const validateForm = () => {
-  const newErrors = {};
-
-  if (!form.name.trim()) newErrors.name = "Required";
-  if (!form.fatherName.trim()) newErrors.fatherName = "Required";
-  if (!form.class) newErrors.class = "Required";
-  if (!form.dob) newErrors.dob = "Required";
-  if (!form.gender) newErrors.gender = "Required";
-  if (!form.mobile) newErrors.mobile = "Required";
-  if (form.mobile && form.mobile.length !== 10)
-    newErrors.mobile = "Must be 10 digits";
-
-  if (!form.address.trim()) newErrors.address = "Required";
-
-  if (form.paymentStatus === "paid" && !form.fee) {
-    newErrors.fee = "Required";
+    return;
   }
 
-  setErrors(newErrors);
-
-  return newErrors;
+  setForm({
+    ...form,
+    [name]: type === "checkbox" ? checked : value,
+  });
 };
+
+
 
   //  SUBMIT (ADD + UPDATE)
   const handleSubmit = async (e) => {
+      if (!e.target.checkValidity()) return;
     e.preventDefault();
 
-  const validationErrors = validateForm();
 
-  if (Object.keys(validationErrors).length > 0) {
-    // 🔥 first error field focus
-    const firstField = Object.keys(validationErrors)[0];
-    document.querySelector(`[name="${firstField}"]`)?.focus();
-    return;
-  }
 
     const url = selectedStudent
       ? `http://localhost:5001/api/students/${selectedStudent._id}`
@@ -336,14 +328,19 @@ const validateForm = () => {
     const method = selectedStudent ? "PUT" : "POST";
 
     const finalData = {
-      ...form,
-      documents: {
-        photo: form.photo,
-        birthCert: form.birthCert,
-        aadhar: form.aadhar,
-        tc: form.tc,
-      },
-    };
+  ...form,
+  admissionFeeAmount:
+    form.paymentStatus === "paid" ? form.fee : "",
+  paymentMode:
+    form.paymentStatus === "paid" ? form.paymentMode : "",
+
+  documents: {
+    photo: form.photo,
+    birthCert: form.birthCert,
+    aadhar: form.aadhar,
+    tc: form.tc,
+  },
+};
 
     try {
       const res = await fetch(url, {
@@ -392,21 +389,32 @@ const validateForm = () => {
               key={s._id}
               className="p-2 hover:bg-gray-100 cursor-pointer"
               onClick={() => {
-  setForm({
-    ...s,
+//   setForm({
+//     ...s,
 
-    dob: s.dob ? s.dob.split("T")[0] : "",
-    admissionDate: s.admissionDate
-      ? s.admissionDate.split("T")[0]
-      : "",
+//     dob: s.dob ? s.dob.split("T")[0] : "",
+//     admissionDate: s.admissionDate
+//       ? s.admissionDate.split("T")[0]
+//       : "",
 
-    paymentStatus: s.admissionFeeStatus || "unpaid",
-fee: s.admissionFeeAmount || "",
-paymentMode: s.paymentMode || "cash",
-    fee: s.admissionFeeAmount,
-    ...s.documents,
-  });
+//     paymentStatus: s.admissionFeeStatus || "unpaid",
+// fee: s.admissionFeeAmount || "",
+// paymentMode: s.paymentMode || "cash",
+//     fee: s.admissionFeeAmount,
+//     ...s.documents,
+//   });
 
+setForm({
+  ...s,
+  dob: s.dob ? s.dob.split("T")[0] : "",
+  admissionDate: s.admissionDate ? s.admissionDate.split("T")[0] : "",
+
+  paymentStatus: s.admissionFeeStatus || "unpaid",
+  fee: s.admissionFeeAmount || "",
+  paymentMode: s.paymentMode || "cash",
+
+  ...s.documents,
+});
   setSelectedStudent(s);
   setStudents([]);
   setSearch("");
@@ -425,22 +433,28 @@ paymentMode: s.paymentMode || "cash",
           <h2 className="text-lg font-semibold mb-4">Student Details</h2>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-            <Input label="Student Name *" name="name" value={form.name}  error={errors.name} onChange={handleChange} />
-            <Input label="Father Name *" name="fatherName" value={form.fatherName}  error={errors.fatherName} onChange={handleChange} />
-            <Input label="Mother Name *" name="motherName" value={form.motherName}  error={errors.motherName} onChange={handleChange} />
-            <Input type="date" label="Date of Birth *" name="dob" value={form.dob}  error={errors.dob} onChange={handleChange} />
+            <Input label="Student Name *" name="name" value={form.name}  required onChange={handleChange} />
+            <Input label="Father Name *" name="fatherName" value={form.fatherName}  required onChange={handleChange} />
+            <Input label="Mother Name *" name="motherName" value={form.motherName}  required onChange={handleChange} />
+            <Input type="date" label="Date of Birth *" name="dob" value={form.dob}  required onChange={handleChange} />
 
-            <Select label="Gender *" name="gender" value={form.gender}   className={`input ${errors.gender ? "border-red-500" : ""}`} onChange={handleChange} options={["Male", "Female"]} />
-            <Select label="Class *" name="class" value={form.class}   className={`input ${errors.class ? "border-red-500" : ""}`}onChange={handleChange} options={["Nursery","LKG","UKG","1","2","3","4","5","6","7","8"]} />
+            <Select label="Gender *" name="gender" value={form.gender}   required onChange={handleChange} options={["Male", "Female"]} />
+            <Select label="Class *" name="class" value={form.class}  required  onChange={handleChange} options={["Nursery","LKG","UKG","1","2","3","4","5","6","7","8"]} />
 
-            <Input type="number" label="Mobile Number *" name="mobile" value={form.mobile}  error={errors.mobile} onChange={handleChange} />
-            <Input type="number" label="Alternate Mobile" name="alternateMobile" value={form.alternateMobile}   onChange={handleChange} />
-            <Input type="date" label="Admission Date *" name="admissionDate" value={form.admissionDate}  error={errors.admissionDate} onChange={handleChange} />
+            <Input type="tel" maxLength={10} label="Mobile Number *" name="mobile" value={form.mobile}  required onChange={handleChange} />
+            <Input type="tel" maxLength={10} label="Alternate Mobile (optional)" name="alternateMobile" value={form.alternateMobile}   onChange={handleChange} />
+            <Input type="date" label="Admission Date *" name="admissionDate" value={form.admissionDate}  required onChange={handleChange} />
           </div>
 
           <div className="mt-4">
             <label className="label">Address *</label>
-            <textarea name="address" value={form.address} className={`input ${errors.address ? "border-red-500" : ""}`} onChange={handleChange}/>
+            <textarea
+  name="address"
+  value={form.address}
+  required
+  onChange={handleChange}
+  className="input"
+/>
           </div>
         </div>
 
@@ -462,11 +476,11 @@ paymentMode: s.paymentMode || "cash",
             <Select label="Payment Status" name="paymentStatus" value={form.paymentStatus} onChange={handleChange} options={["paid","unpaid"]} />
 
             {form.paymentStatus === "paid" && (
-              <Input type="number" label="Fee Amount" name="fee" value={form.fee} onChange={handleChange} />
+              <Input type="number" label="Fee Amount *" name="fee" value={form.fee} required onChange={handleChange} />
             )}
 
             {form.paymentStatus === "paid" && (
-              <Select label="Payment Mode" name="paymentMode" value={form.paymentMode} onChange={handleChange} options={["cash","upi"]} />
+              <Select label="Payment Mode *" name="paymentMode" value={form.paymentMode} required onChange={handleChange} options={["cash","upi"]} />
             )}
           </div>
         </div>
@@ -492,8 +506,7 @@ paymentMode: s.paymentMode || "cash",
 }
 
 /* COMPONENTS */
-
-function Input({ label, type, error, ...props }) {
+function Input({ label, type, ...props }) {
   return (
     <div>
       <label className="label">{label}</label>
@@ -501,10 +514,8 @@ function Input({ label, type, error, ...props }) {
       <input
         type={type}
         {...props}
-        className={`input ${type === "number" ? "text-right" : ""} ${error ? "border-red-500" : ""}`}
+        className={`input ${type === "number" || type === "tel" ? "text-right" : ""}`}
       />
-
-      {error && <p className="text-red-500 text-xs mt-1">{error}</p>}
     </div>
   );
 }
@@ -513,6 +524,7 @@ function Select({ label, options, ...props }) {
   return (
     <div>
       <label className="label">{label}</label>
+
       <select {...props} className="input">
         <option value="">Select</option>
         {options.map((opt, i) => (
